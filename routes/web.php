8e556e988cf2bcb353b37423d3eedd56aa6eb5c0
HuCiao <1476559227@qq.com>
2021-09-20 17:30:23 +0800

    订单超时取消

--- routes/web.php
+++ routes/web.php
@@ -58,6 +58,9 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::get('cart', 'CartController@index')->name('cart.index');
     //商品移出购物车
     Route::delete('cart/{sku}', 'CartController@remove')->name('cart.remove');
+
+    //购物车下订单
+    Route::post('orders', 'OrdersController@store')->name('orders.store');
 });
 
 Route::redirect('/', '/products')->name('root');
